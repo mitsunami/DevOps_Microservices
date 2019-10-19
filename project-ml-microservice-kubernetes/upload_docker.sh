18060dfff9b0866e04c0ba4a4889778cf7893b82
Koki Mitsunami <k.mitsunami@gmail.com>
2019-10-19 07:06:42 +0100

    updte upload_docker.sh

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=kmitsunami/mlservice
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username kmitsunami
+docker tag f7b102e8aa4b kmitsunami/mlservice:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push kmitsunami/mlservice:latest
